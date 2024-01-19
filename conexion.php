d20ce988f5cf76b1e0dbd3ef925f1c4922951b5e
Kevin Jofraly Pérez López <jofraly_pl@tesch.edu.mx>
2024-01-18 23:33:48 -0600

    Se añadió el código para conectar con la BD en el archivo conexion.php

---
+++ conexion.php
@@ -0,0 +1,34 @@
+<?php
+class Conexion
+{
+    private string $server = "mysql:host=localhost; dbname=agenda";
+    private string $user  = "root";
+    private string $password = "";
+    private array $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ];
+    protected PDO $conn;
+
+    public function open()
+    {
+        try {
+            //code...
+            $this->conn = new PDO(
+                $this->server,
+                $this->user,
+                $this->password,
+                $this->options
+            );
+
+            return $this->conn;
+        } catch (PDOException $e) {
+            echo "Error al conectar a la base de datos: " . $e->getMessage();
+        }
+    }
+
+    public function close()
+    {
+        $this->conn = null;
+    }
+}
