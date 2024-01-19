035dc3a37a40fb18268bc3326b9c4267216c7207
Kevin Jofraly Pérez López <jofraly_pl@tesch.edu.mx>
2024-01-18 22:51:57 -0600

    Se crearon los campos y botones del formulario modal

--- modal.php
+++ modal.php
@@ -10,6 +10,58 @@
                 </button>
             </div>
             <!-- Add the rest of your modal body here -->
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <form action="#" method="post">
+                        <div class="row form-group">
+                            <div class="col-sm-2">
+                                <label class="control-label">Nombre: </label>
+                            </div>
+                            <div class="col-sm-10">
+                                <input type="text" name="nombre" id="name" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-sm-2">
+                                <label class="control-label">Télefono: </label>
+                            </div>
+                            <div class="col-sm-10">
+                                <input type="tel" name="tel" id="phone" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-sm-2">
+                                <label class="control-label">Correo: </label>
+                            </div>
+                            <div class="col-sm-10">
+                                <input type="email" name="correo" id="mail" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-sm-2">
+                                <label class="control-label">Dirección: </label>
+                            </div>
+                            <div class="col-sm-10">
+                                <input type="text" name="dor" id="address" class="form-control">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">
+                                <span class="fa fa-close">
+                                    &nbsp;
+                                </span>
+                                Cancelar
+                            </button>
+                            <button type="submit" class="btn btn-primary">
+                                <span class="fa fa-save">
+                                    &nbsp;
+                                </span>
+                                Guardar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
